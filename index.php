b065b23dc6cc3f4bc13ac33a5a0ff0d640092949
Taha Mohamed <taha.mohamed2016@gmail.com>
2023-05-06 02:21:26 +0300

    Array

--- index.php
+++ index.php
@@ -6,15 +6,29 @@
 </head>
 <body>
     <h1>
-        <?php
-            echo "Hello, World"
-        ?>
-        <br>
-        <?= "Hello, World"?>
-        <br>
-        <?php
-            print "Hello, World";
-        ?>
+        Recommended Books
     </h1>
+
+    <?php
+        $books = [
+            "Do Androids Dream of Electric Sheep",
+            "The Langoliers",
+            "Hail Mary"
+        ];
+    ?>
+
+    <ul>
+        <?php
+            foreach ($books as $book){
+                echo "<li>{$book}</li>";
+            }
+        ?>
+    </ul>
+    <hr>
+    <ul>
+        <?php foreach ($books as $book): ?>
+            <li><?= $book ?></li>
+        <?php endforeach;?>
+    </ul>
 </body>
 </html>
