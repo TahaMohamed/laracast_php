26ff0b082a5fc24d669536a714d4a753fb8a081e
Taha Mohamed <taha.mohamed2016@gmail.com>
2023-05-06 19:18:12 +0300

    SQL injection vulnerability EP20

--- index.php
+++ index.php
@@ -9,9 +9,11 @@ $configs = require 'config.php';
 
 $db = new Database($configs['database']);
 $id = $_GET['id'];
-$query ="SELECT * FROM posts WHERE id = ?";
+$query ="SELECT * FROM posts WHERE id = :id";
 
-$posts = $db->query($query, [$id])->fetch();
+$posts = $db->query($query, ['id' => $id])->fetch();
+//OR
+//$posts = $db->query($query, [':id' => $id])->fetch();
 
 dd($posts);
 
