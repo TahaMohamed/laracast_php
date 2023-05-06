ff033b76ffa89b132778d005217542b2c05eb194
Taha Mohamed <taha.mohamed2016@gmail.com>
2023-05-06 17:48:10 +0300

    Coonect to Database PDO EP16

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 
-require 'functions.php';
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 
-require 'router.php';
+require 'functions.php';
+//require 'router.php';
+// Connect to MySQL database
+$dsn = 'mysql:host=localhost;port=3306;dbname=laracasts_php;user=root;password=1;charset=utf8mb4;unix_socket=/tmp/mysql.sock';
+$pdo = new PDO($dsn);
+$statement = $pdo->prepare("select * from posts");
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
